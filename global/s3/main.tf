59901a0fc4c99db6cec23a4017606c1ddc724e98
Yash Jagani <yashjagani17@gmail.com>
2026-08-03 11:26:36 +0100

    backend s3 state configuration

---
+++ global/s3/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "yash-jagani-portfolio-terraform-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+terraform {
+    backend "s3" {
+      bucket = "yash-jagani-portfolio-terraform-state"
+      key = "global/s3/terraform.tfstate"
+      region = "eu-west-2"
+      encrypt = true
+      use_lockfile = true
+    }
+}
